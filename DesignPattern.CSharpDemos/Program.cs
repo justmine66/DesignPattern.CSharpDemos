eb202d9b80f4d70f48e51ccea1d68fa0e992548a
justmine <justmine@DESKTOP-UILU6SC>
2017-12-30 20:52:40 -0800

    Add project files.

---
+++ DesignPattern.CSharpDemos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPattern.CSharpDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
